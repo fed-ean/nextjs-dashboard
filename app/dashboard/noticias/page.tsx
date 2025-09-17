8105adb7d91e2feab3faaef509eb9168fbe14124
Federico Nuñez <nfede41@gmail.com>
2025-09-17 10:46:14 -0300

    Co-authored-by: Midulio <Midulio@users.noreply.github.com> Co-authored-by: PriscillaAguirre <PriscillaAguirre@users.noreply.github.com>

---
+++ app/dashboard/noticias/page.tsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NoticiaEncabezado from '../../ui/dashboard/noticia-encabezado';
+
+export default function PageNoticias() {
+    return(
+        <main>
+            <NoticiaEncabezado />
+
+            
+        </main>
+    )
+}
