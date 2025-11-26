51d154f0f5d3a330aeb8ad802f860613520bf1f8
nfede41 <nfede41@gmail.com>
2025-11-26 16:39:47 +0000

    correcion

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,10 @@
 import type { Metadata } from "next";
 import { Alegreya_Sans } from "next/font/google";
 import "./global.css";
-import dynamic from "next/dynamic";
 
-// ⛔ IMPORTANTE: los cargamos sin SSR
-const NavBar = dynamic(() => import("@/app/ui/Page_Index/navbar"), { ssr: false });
-const EnVivoLayout = dynamic(() => import("@/app/ui/EnVivoLayout"), { ssr: false });
-const Footer = dynamic(() => import("@/app/ui/Page_Index/footer"), { ssr: false });
+import NavBar from "@/app/ui/Page_Index/navbar";
+import EnVivoLayout from "@/app/ui/EnVivoLayout";
+import Footer from "@/app/ui/Page_Index/footer";
 
 const alegreyaSans = Alegreya_Sans({
   subsets: ["latin"],
@@ -26,11 +24,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={`${alegreyaSans.variable} font-sans antialiased flex flex-col h-full`}>
         <EnVivoLayout />
         <NavBar />
-
         <main className="flex-grow pt-28">
           {children}
         </main>
-
         <Footer />
       </body>
     </html>
