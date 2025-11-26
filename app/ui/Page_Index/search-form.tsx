a7930e7acd20645db359e1551bf10eb35d141b92
nfede41 <nfede41@gmail.com>
2025-11-26 17:07:52 +0000

    prueba

--- app/ui/Page_Index/search-form.tsx
+++ app/ui/Page_Index/search-form.tsx
@@ -4,19 +4,20 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
-export default function SearchForm({ placeholder }: { placeholder: string }) {
+export default function SearchFormInner({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    
     if (term) {
       params.set('q', term);
     } else {
       params.delete('q');
     }
+
     replace(`/Noticias/buscar?${params.toString()}`);
   }, 300);
 
@@ -25,15 +26,18 @@ export default function SearchForm({ placeholder }: { placeholder: string }) {
       <label htmlFor="search" className="sr-only">
         Search
       </label>
+
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
-        defaultValue={searchParams.get('q')?.toString()}
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('q') ?? ''}
       />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+
+      <MagnifyingGlassIcon 
+        className="absolute left-3 top-1/2 h-[18px] w-[18px] 
+                   -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" 
+      />
     </div>
   );
 }
