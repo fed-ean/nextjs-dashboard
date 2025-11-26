37af729ccec1882c88a4d18777894af08247c2a9
nfede41 <nfede41@gmail.com>
2025-11-26 15:52:02 +0000

    corrreciones del sidenav

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -81,3 +81,26 @@ export function GridNoticiasPrincipalesSkeleton() {
       </div>
     );
   }
+  export function NoticiaIndividualSkeleton() {
+    return (
+      <div className="max-w-4xl mx-auto p-6 space-y-6 animate-pulse">
+        {/* Título */}
+        <div className="h-10 bg-gray-300 rounded w-3/4"></div>
+  
+        {/* Imagen principal */}
+        <div className="w-full h-64 bg-gray-300 rounded-lg"></div>
+  
+        {/* Meta info */}
+        <div className="h-4 bg-gray-300 rounded w-1/3"></div>
+  
+        {/* Contenido */}
+        <div className="space-y-4">
+          <div className="h-4 bg-gray-300 rounded w-full"></div>
+          <div className="h-4 bg-gray-300 rounded w-11/12"></div>
+          <div className="h-4 bg-gray-300 rounded w-10/12"></div>
+          <div className="h-4 bg-gray-300 rounded w-9/12"></div>
+        </div>
+      </div>
+    );
+  }
+  
