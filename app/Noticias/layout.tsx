4df64527cb5571d7708bbd5b99c23a0f9eeb5b25
nfede41 <nfede41@gmail.com>
2025-11-26 16:08:06 +0000

    Correcciones

--- app/Noticias/layout.tsx
+++ app/Noticias/layout.tsx
@@ -1,20 +1,23 @@
+// app/Noticias/layout.tsx
 
 import SideNav from "../ui/Page_Index/sidenav";
-import "../global.css";
+import { getAllCategories } from "../lib/data-fetcher";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const categories = await getAllCategories();
+
   return (
     <div className="flex h-auto flex-col md:flex-row md:overflow-hidden">
-        <div className="w-full flex-none md:w-64">
-            <SideNav/>
-        </div>
-        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-            {children}
-        </div>
+      <div className="w-full flex-none md:w-64">
+        <SideNav categories={categories} />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+        {children}
+      </div>
     </div>
   );
 }
