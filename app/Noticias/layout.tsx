3a2b74a110c532eaabaf4a41ac7dd3650845fcc1
nfede41 <nfede41@gmail.com>
2025-11-28 01:49:19 +0000

    modificacion

--- app/Noticias/layout.tsx
+++ app/Noticias/layout.tsx
@@ -1,22 +1,23 @@
-// app/Noticias/layout.tsx
 
-import SideNav from "../ui/Page_Index/sidenav";
-import { getAllCategories } from "../lib/data-fetcher";
+import SidenavServer from "@/app/ui/Page_Index/SidenavServer";
 
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const categories = await getAllCategories();
-
+export default function NoticiasLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-auto flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
-        <SideNav categories={categories} />
-      </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-        {children}
+    <div className="container mx-auto px-4 py-8">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        
+        {/* Columna Izquierda: Sidenav */}
+        <aside className="lg:col-span-3 space-y-8">
+          <div className="sticky top-24">
+            <SidenavServer />
+          </div>
+        </aside>
+
+        {/* Columna Derecha: Contenido de la página (ej. el post) */}
+        <main className="lg:col-span-9">
+          {children}
+        </main>
+
       </div>
     </div>
   );
