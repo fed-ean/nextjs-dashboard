6b9cf8298b60ac8254cdfeb2428f5dbc79ddbcf8
nfede41 <nfede41@gmail.com>
2025-12-03 03:13:48 +0000

    HOla

--- app/Categorias/[slug]/page.tsx
+++ app/Categorias/[slug]/page.tsx
@@ -46,6 +46,7 @@ export default async function CategoriaPage(
   if (found) categoryName = found.name;
 
   if (!posts || posts.length === 0) {
+    
     return (
       <div className="max-w-7xl mx-auto p-4">
         <h1 className="text-3xl font-bold mb-4">{categoryName}</h1>
